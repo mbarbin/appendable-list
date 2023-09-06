8ae7f021e811cb49fbd6ca28bba7d239c59b1296
Mathieu Barbin <mathieu.barbin@gmail.com>
2023-09-06 09:32:25 +0200

    import code

--- src/appendable_list.ml
+++ src/appendable_list.ml
@@ -1,1 +1,259 @@
 open! Base
+
+(* A Stack that supports an efficient push_list operation *)
+module List_stack : sig
+  type 'a t
+
+  val is_empty : _ t -> bool
+  val pop_exn : 'a t -> 'a
+  val push_list : 'a t -> 'a list -> unit
+  val singleton : 'a -> 'a t
+end = struct
+  type 'a t = 'a list Stack.t
+
+  let _invariant t = Stack.iter t ~f:(fun l -> assert (not (List.is_empty l)))
+  let singleton x = Stack.singleton [ x ]
+  let is_empty t = Stack.is_empty t
+
+  let push_list t = function
+    | [] -> ()
+    | _ :: _ as list -> Stack.push t list
+  ;;
+
+  let rec pop_exn t =
+    match Stack.pop_exn t with
+    | [] -> pop_exn t
+    | hd :: tl ->
+      push_list t tl;
+      hd
+  ;;
+end
+
+type +'a t =
+  | Empty
+  | Singleton of 'a
+  | List of 'a * 'a * 'a list
+  | Node of 'a t * 'a t * 'a t list
+[@@deriving sexp_of]
+
+let empty = Empty
+
+let fold_left =
+  let rec go todo ~init ~f =
+    match todo with
+    | [] -> init
+    | Empty :: todo -> go todo ~init ~f
+    | Singleton a :: todo -> go todo ~init:(f init a) ~f
+    | List (a, b, cs) :: todo -> go todo ~init:(List.fold ~f ~init:(f (f init a) b) cs) ~f
+    | Node (a, b, cs) :: todo -> go (a :: b :: (cs @ todo)) ~init ~f
+  in
+  fun t ~init ~f -> (go [ t ] ~init ~f [@nontail])
+;;
+
+let fold_right =
+  (* [todo] is the stack of the remaining work, viewed from the right, so [hd todo] is
+     the rightmost node that was not processed yet.
+
+     Right-to-left evaluation of the original appendable list is then accomplished by
+     traversing [todo] from head to tail. We still need to traverse each element of
+     [todo] from right to left. *)
+  let rec go todo ~init ~f =
+    match todo with
+    | [] -> init
+    | Empty :: todo -> go todo ~init ~f
+    | Singleton x :: todo -> go todo ~init:(f x init) ~f
+    | List (a, b, cs) :: todo ->
+      let init = List.fold_right ~f cs ~init in
+      let init = f b init in
+      let init = f a init in
+      go todo ~init ~f
+    | Node (a, b, cs) :: todo -> go (List.rev_append cs (b :: a :: todo)) ~init ~f
+  in
+  fun t ~init ~f -> go [ t ] ~init ~f
+;;
+
+let fold = fold_left
+let iter t ~f = Container.iter ~fold ~f t
+
+let rec is_empty = function
+  | Empty -> true
+  | Singleton _ | List _ -> false
+  | Node (a, b, list) -> is_empty a && is_empty b && List.for_all list ~f:is_empty
+;;
+
+let fold_result t ~init ~f = Container.fold_result ~fold ~init ~f t
+let fold_until t ~init ~f ~finish = Container.fold_until ~fold ~init ~f t ~finish
+let length t = fold t ~init:0 ~f:(fun acc _ -> Int.succ acc)
+let to_list t = fold_right t ~init:[] ~f:(fun x acc -> x :: acc)
+
+(* Mauro Jaskelioff and Exequiel Rivas. 2015. Functional pearl: a smart view on datatypes.
+   SIGPLAN Not. 50, 9 (August 2015), 355-361.
+   DOI=http://dx.doi.org/10.1145/2858949.2784743
+   https://www.fceia.unr.edu.ar/~mauro/pubs/smartviews/smartviews.pdf *)
+let rec next = function
+  | Empty -> None
+  | Singleton a -> Some (a, Empty)
+  | List (a, b, cs) ->
+    Some
+      ( a
+      , match cs with
+        | [] -> Singleton b
+        | hd :: tl -> List (b, hd, tl) )
+  | Node (Node (a, b, cs), d, es) ->
+    (* amortize the traversal of [a] *)
+    next
+      (match cs with
+       | [] -> Node (a, b, d :: es)
+       | [ x ] -> Node (a, b, x :: d :: es)
+       | x :: y :: tl -> Node (a, b, Node (x, y, tl) :: d :: es))
+  | Node (Empty, b, cs) ->
+    next
+      (match cs with
+       | [] -> b
+       | hd :: tl -> Node (b, hd, tl))
+  | Node (Singleton a, b, cs) ->
+    Some
+      ( a
+      , match cs with
+        | [] -> b
+        | hd :: tl -> Node (b, hd, tl) )
+  | Node (List (a, b, cs), d, es) ->
+    let b =
+      match cs with
+      | [] -> Singleton b
+      | hd :: tl -> List (b, hd, tl)
+    in
+    Some (a, Node (b, d, es))
+;;
+
+let to_sequence t = Sequence.unfold ~init:t ~f:next
+
+let concat = function
+  | [] -> Empty
+  | [ t ] -> t
+  | a :: b :: c -> Node (a, b, c)
+;;
+
+let append t1 t2 = Node (t1, t2, [])
+let add_front x t = Node (Singleton x, t, [])
+let add_back t x = Node (t, Singleton x, [])
+
+let of_list = function
+  | [] -> Empty
+  | [ x ] -> Singleton x
+  | a :: b :: c -> List (a, b, c)
+;;
+
+let singleton x = Singleton x
+
+let rec non_closure_apply_and_tail_rec_map nodes acc ~f =
+  if List_stack.is_empty nodes
+  then acc
+  else (
+    let acc =
+      match List_stack.pop_exn nodes with
+      | Empty -> acc
+      | Singleton x -> append acc (Singleton (f x))
+      | List (a, b, cs) ->
+        let a = f a in
+        let b = f b in
+        let cs = List.map cs ~f in
+        append acc (List (a, b, cs))
+      | Node (a, b, cs) ->
+        List_stack.push_list nodes (a :: b :: cs);
+        acc
+    in
+    non_closure_apply_and_tail_rec_map nodes acc ~f)
+;;
+
+let map t ~f = non_closure_apply_and_tail_rec_map (List_stack.singleton t) empty ~f
+
+let rec non_closure_apply_and_tail_rec_bind nodes acc ~f =
+  if List_stack.is_empty nodes
+  then acc
+  else (
+    let acc =
+      match List_stack.pop_exn nodes with
+      | Empty -> acc
+      | Singleton x -> append acc (f x)
+      | List (a, b, cs) ->
+        let a = f a in
+        let b = f b in
+        let cs = List.map cs ~f in
+        Node (acc, a, b :: cs)
+      | Node (a, b, cs) ->
+        List_stack.push_list nodes (a :: b :: cs);
+        acc
+    in
+    non_closure_apply_and_tail_rec_bind nodes acc ~f)
+;;
+
+let bind t ~f = non_closure_apply_and_tail_rec_bind (List_stack.singleton t) empty ~f
+
+let sexp_of_t (type a) (sexp_of_a : a -> Sexp.t) (t : a t) =
+  to_list t |> [%sexp_of: a list]
+;;
+
+let t_of_sexp (type a) (a_of_sexp : Sexp.t -> a) sexp =
+  sexp |> [%of_sexp: a list] |> of_list
+;;
+
+(* Below: we use a pattern that makes explicit how to complete the interfaces with
+   functions that do not have a more efficient implementation in that module already *)
+
+include struct
+  module Container_gen = Container.Make (struct
+      type nonrec 'a t = 'a t
+
+      let fold = fold
+      let iter = `Custom iter
+      let length = `Custom length
+    end)
+
+  open Container_gen
+
+  let count = count
+  let exists = exists
+  let find = find
+  let find_map = find_map
+  let for_all = for_all
+  let min_elt = min_elt
+  let max_elt = max_elt
+  let mem = mem
+  let to_array = to_array
+  let sum = sum
+end
+
+include struct
+  module Monad_gen = Monad.Make (struct
+      type nonrec 'a t = 'a t
+
+      let return = singleton
+      let map = `Custom map
+      let bind = bind
+    end)
+
+  open Monad_gen
+  module Monad_infix = Monad_infix
+  module Let_syntax = Let_syntax
+
+  let ignore_m = ignore_m
+  let join = join
+  let bind = bind
+  let ( >>= ) t f = bind t ~f
+  let ( >>| ) = ( >>| )
+  let return = return
+  let all = all
+  let all_unit = all_unit
+end
+
+(* A reference implementation of [map] that is perhaps not as efficient but more
+   obviously correct. *)
+let rec map_simple t ~f =
+  match t with
+  | Empty -> Empty
+  | Singleton a -> Singleton (f a)
+  | List (a, b, cs) -> List (f a, f b, List.map cs ~f)
+  | Node (a, b, cs) ->
+    Node (map_simple a ~f, map_simple b ~f, List.map cs ~f:(map_simple ~f))
+;;
